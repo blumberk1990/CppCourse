c60a4a997d90819233867eea037472b16fbc615c
blumberk1990 <magda.hubert.magda@gmail.com>
2022-02-09 12:38:30 +0100

    for each loop, C++V11

---
+++ Lesson_04/01_display/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int A[]={2,4,6,8,10};
+	
+    for(auto x:A) {
+		cout << x << " ";
+	}
+    cout << "\n";
+	
+	return 0;
+}
