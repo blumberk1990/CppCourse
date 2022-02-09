e7320c1fb8e9300e557dc64df9e93c83c380adfd
blumberk1990 <magda.hubert.magda@gmail.com>
2022-02-09 12:59:56 +0100

    sum, and max value

--- Lesson_04/01_display/main.cpp
+++ Lesson_04/01_display/main.cpp
@@ -4,12 +4,27 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	
-	int A[]={2,4,6,8,10};
+	int A[] = {2, 4, 6, 8, 10, 1};
+	int sum = 0;
+	int max = 0;
+
+	// ex_1: display, using for each loop	
+	// ex_2: sum of array
+	// ex_3: max value of array
 	
     for(auto x:A) {
-		cout << x << " ";
-	}
-    cout << "\n";
+    
+		if(x > max) {
+    		max = x;
+		}
 	
+    	sum = sum + x;
+	
+		cout << x << " ";
+	
+	}
+    
+	cout << "\n" << "sum of A is equal to: " << sum << endl;
+	cout << "max value of A array is: " << max << endl;
 	return 0;
 }
