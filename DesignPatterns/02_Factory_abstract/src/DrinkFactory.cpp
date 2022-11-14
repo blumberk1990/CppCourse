00473d351f1e833cac680e06bb5e33db001995e2
Hubert Magda <magda.hubert.magda@gmail.com>
2022-11-14 21:21:14 +0100

    lambda

--- DesignPatterns/02_Factory_abstract/src/DrinkFactory.cpp
+++ DesignPatterns/02_Factory_abstract/src/DrinkFactory.cpp
@@ -12,3 +12,24 @@ std::unique_ptr<HotDrink> DrinkFactory::makeDrink(const std::string& name)
     drink->prepare(150);
     return drink;
 }
+
+DrinkWithVolumeFactory::DrinkWithVolumeFactory()
+{
+    // lambda function body invoked lambda function
+    hotDrinkFactories["tea"] = [] {
+        auto drink = std::make_unique<Tea>();
+        drink->prepare(275);
+        return drink;
+    };
+    // lambda function body invoked lambda function
+    hotDrinkFactories["coffee"] = [] {
+        auto drink = std::make_unique<Coffee>();
+        drink->prepare(275);
+        return drink;
+    };
+}
+
+std::unique_ptr<HotDrink> DrinkWithVolumeFactory::makeDrink(const std::string& name)
+{
+    return hotDrinkFactories[name]();
+}
